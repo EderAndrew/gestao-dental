6123af49516a48f7c3a17d4ef4cf52188394e8ac
Eder Andrew <ederandrew0028@gmail.com>
2024-09-20 15:09:47 -0300

    update office form

---
+++ prisma/migrations/20240920174938_update_addresses/migration.sql
@@ -0,0 +1,228 @@
+-- CreateEnum
+CREATE TYPE "Role" AS ENUM ('USER', 'ADMIN', 'BACKOFFICE', 'PATIENT');
+
+-- CreateTable
+CREATE TABLE "addresses" (
+    "id" SERIAL NOT NULL,
+    "street" TEXT NOT NULL,
+    "number" TEXT NOT NULL,
+    "complement" TEXT,
+    "neighborhood" TEXT NOT NULL,
+    "city" TEXT NOT NULL,
+    "state" TEXT NOT NULL,
+    "cep" TEXT NOT NULL,
+    "userId" INTEGER NOT NULL,
+    "officeId" TEXT,
+    "patientId" TEXT,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "addresses_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "anamneses" (
+    "id" SERIAL NOT NULL,
+    "reason" TEXT NOT NULL,
+    "discomfort_mouth" TEXT NOT NULL,
+    "high_pressure" TEXT NOT NULL,
+    "control_pressure" TEXT NOT NULL,
+    "oral_hygiene" TEXT NOT NULL,
+    "grind_teeth" TEXT NOT NULL,
+    "allergy" TEXT NOT NULL,
+    "what_allergy" TEXT NOT NULL,
+    "drink_smoke" TEXT NOT NULL,
+    "frequency" TEXT NOT NULL,
+    "bleeding" TEXT NOT NULL,
+    "when_bleeding" TEXT NOT NULL,
+    "sensitivity" TEXT NOT NULL,
+    "prothesis" TEXT NOT NULL,
+    "prothesis_type" TEXT NOT NULL,
+    "pregnant_breastfeeding" TEXT,
+    "pregnant_time" TEXT,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "patientId" INTEGER NOT NULL,
+
+    CONSTRAINT "anamneses_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "budgets" (
+    "id" SERIAL NOT NULL,
+    "treatment_type" TEXT NOT NULL,
+    "treatment_date" TEXT NOT NULL,
+    "procedure_type" TEXT NOT NULL,
+    "agreement" TEXT NOT NULL,
+    "total" INTEGER NOT NULL,
+    "entry" INTEGER,
+    "pay_installments" INTEGER NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "patientId" INTEGER NOT NULL,
+
+    CONSTRAINT "budgets_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "offices" (
+    "id" SERIAL NOT NULL,
+    "identity" TEXT,
+    "corporate" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "tel" TEXT NOT NULL,
+    "cnpj" TEXT NOT NULL,
+    "status" BOOLEAN NOT NULL DEFAULT true,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "offices_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "patients" (
+    "id" SERIAL NOT NULL,
+    "identity" TEXT,
+    "name" TEXT NOT NULL,
+    "email" TEXT,
+    "cpf" TEXT NOT NULL,
+    "rg" TEXT NOT NULL,
+    "birth_date" TEXT NOT NULL,
+    "tel" TEXT NOT NULL,
+    "career" TEXT NOT NULL,
+    "gender" TEXT NOT NULL,
+    "age" INTEGER NOT NULL,
+    "responsible_name" TEXT,
+    "responsible_birth_date" TEXT,
+    "contact_name" TEXT,
+    "contact_tel" TEXT,
+    "agreement" TEXT NOT NULL,
+    "agreement_card" TEXT NOT NULL,
+    "holder_name" TEXT,
+    "cpf_holder" TEXT,
+    "status" BOOLEAN NOT NULL DEFAULT true,
+    "role" "Role" NOT NULL DEFAULT 'PATIENT',
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "officeId" INTEGER NOT NULL,
+
+    CONSTRAINT "patients_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "sessions" (
+    "id" SERIAL NOT NULL,
+    "expiresAt" TIMESTAMP(3) NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "timestamps" BOOLEAN NOT NULL DEFAULT true,
+    "userId" INTEGER NOT NULL,
+
+    CONSTRAINT "sessions_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "sessionsAdmin" (
+    "id" SERIAL NOT NULL,
+    "expiresAt" TIMESTAMP(3) NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "timestamps" BOOLEAN NOT NULL DEFAULT true,
+    "userAdminId" INTEGER NOT NULL,
+
+    CONSTRAINT "sessionsAdmin_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "teeths" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "position" TEXT NOT NULL,
+    "faces" TEXT NOT NULL,
+    "option" TEXT NOT NULL,
+    "value" INTEGER NOT NULL,
+    "discount" INTEGER NOT NULL,
+    "budgetId" INTEGER NOT NULL,
+
+    CONSTRAINT "teeths_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "users" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "tel" TEXT NOT NULL,
+    "status" BOOLEAN NOT NULL DEFAULT true,
+    "role" "Role" NOT NULL DEFAULT 'USER',
+    "officeId" INTEGER NOT NULL,
+    "firstAccess" BOOLEAN NOT NULL DEFAULT true,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "users_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "usersAdmin" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "status" BOOLEAN NOT NULL DEFAULT true,
+    "role" "Role" NOT NULL DEFAULT 'BACKOFFICE',
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "usersAdmin_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "officesPayment" (
+    "id" SERIAL NOT NULL,
+
+    CONSTRAINT "officesPayment_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "offices_identity_key" ON "offices"("identity");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "patients_identity_key" ON "patients"("identity");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "users_email_key" ON "users"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "usersAdmin_email_key" ON "usersAdmin"("email");
+
+-- AddForeignKey
+ALTER TABLE "addresses" ADD CONSTRAINT "addresses_officeId_fkey" FOREIGN KEY ("officeId") REFERENCES "offices"("identity") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "addresses" ADD CONSTRAINT "addresses_patientId_fkey" FOREIGN KEY ("patientId") REFERENCES "patients"("identity") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "addresses" ADD CONSTRAINT "addresses_userId_fkey" FOREIGN KEY ("userId") REFERENCES "users"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "anamneses" ADD CONSTRAINT "anamneses_patientId_fkey" FOREIGN KEY ("patientId") REFERENCES "patients"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "budgets" ADD CONSTRAINT "budgets_patientId_fkey" FOREIGN KEY ("patientId") REFERENCES "patients"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "patients" ADD CONSTRAINT "patients_officeId_fkey" FOREIGN KEY ("officeId") REFERENCES "offices"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "sessions" ADD CONSTRAINT "sessions_userId_fkey" FOREIGN KEY ("userId") REFERENCES "users"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "sessionsAdmin" ADD CONSTRAINT "sessionsAdmin_userAdminId_fkey" FOREIGN KEY ("userAdminId") REFERENCES "usersAdmin"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "teeths" ADD CONSTRAINT "teeths_budgetId_fkey" FOREIGN KEY ("budgetId") REFERENCES "budgets"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "users" ADD CONSTRAINT "users_officeId_fkey" FOREIGN KEY ("officeId") REFERENCES "offices"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
--- src/services/Offices.ts
+++ src/services/Offices.ts
@@ -1,15 +1,67 @@
 import { IOfficeSchema } from "@/interfaces/IOfficeSchema"
-import { PrismaClient } from "@prisma/client"
+import { Prisma, PrismaClient } from "@prisma/client"
 
 const prisma = new PrismaClient()
 
 export const postOffice = async(offices: IOfficeSchema) => {
-    try{
-        const resp = await prisma.offices.create({
-            data: {
+    const includeAddress = true
+    let office: Prisma.officesCreateInput
 
+    const address: Prisma.addressesCreateWithoutOfficesInput = {
+        cep: offices.cep ? offices.cep : "",
+        street: offices.street ? offices.street : "",
+        complement: offices.complement ? offices.complement : "",
+        number: offices.number ? offices.number : "",
+        neighborhood: offices.neighborhood ? offices.neighborhood : "",
+        city: offices.city ? offices.city : "",
+        state: offices.state ? offices.state : "",
+        updatedAt: new Date().toUTCString()
+    };
+
+    const user: Prisma.usersCreateWithoutOfficesInput = {
+        name: offices.userName,
+        email: offices.email,
+        password: offices.password,
+        tel: offices.tel,
+        status: true,
+        role: "ADMIN",
+        updatedAt: new Date().toUTCString()
+    }
+    try{
+        if(includeAddress){
+            office = {
+                identity: offices.identity,
+                cnpj: offices.cnpj,
+                corporate: offices.corporate,
+                name: offices.name,
+                tel: offices.tel,
+                updatedAt: new Date().toUTCString(),
+                addresses:{
+                    create: address
+                },
+                users: {
+                    create : user
+                }
             }
-        })
+        }else{
+            office = {
+                identity: offices.identity,
+                cnpj: offices.cnpj,
+                corporate: offices.corporate,
+                name: offices.name,
+                tel: offices.tel,
+                updatedAt: new Date().toUTCString(),
+                users: {
+                    create : user
+                }
+            }
+        }
+
+        const resp = await prisma.offices.create({
+            data: office
+         })
+        
+         return resp
     }catch(error){
         if(error instanceof Error){
             console.log(error.message)
